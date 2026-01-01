f405d326aac78a8e6a13894ae0bcce361406fbf9
cscsaba242 <cscsaba242@live.com>
2026-01-01 16:30:07 +0100

    init

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+cp /workspaces/devcont/.devcontainer/95proxies /etc/apt/apt.conf.d/95proxies
+apt-get update
+apt-get install -y docker.io curl build-essential git libssl-dev libdbus-1-dev pkg-config unzip zip
+curl -Lo helm.tar.gz https://get.helm.sh/helm-v3.12.0-linux-amd64.tar.gz
+tar -xzf helm.tar.gz
+mv linux-amd64/helm /usr/local/bin/helm
+rm -rf linux-amd64 helm.tar.gz
+curl -LO https://dl.k8s.io/release/v1.29.0/bin/linux/amd64/kubectl
+chmod +x kubectl
+mv kubectl /usr/local/bin/kubectl
+curl -Lo kind https://kind.sigs.k8s.io/dl/v0.23.0/kind-linux-amd64
+chmod +x kind
+mv kind /usr/local/bin/kind
+curl -Lo k9s.tar.gz https://github.com/derailed/k9s/releases/download/v0.32.4/k9s_Linux_amd64.tar.gz
+tar -xzf k9s.tar.gz
+chmod +x k9s
+mv k9s /usr/local/bin/k9s
+rm k9s.tar.gz
+curl https://sh.rustup.rs -sSf | sh
+echo 'source $HOME/.cargo/env' >> ~/.bashrc
+source $HOME/.cargo/env
+cargo install edc-connector-tui
+echo 'source "$HOME/.sdkman/bin/sdkman-init.sh"' >> ~/.bashrc
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk env install
+curl https://logdy.dev/install-silent.sh | sh
